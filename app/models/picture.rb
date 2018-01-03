1933f2f1fbb9c87cfed8395a98543bcad5e802f4
Raghu Betina <raghu@firstdraft.com>
2018-01-03 16:39:58 +0000

    Direct association between pictures and picture_tags

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,9 @@
 class Picture < ApplicationRecord
   # Direct associations
 
+  has_many   :picture_tags,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/picture_tag.rb
+++ app/models/picture_tag.rb
@@ -1,6 +1,8 @@
 class PictureTag < ApplicationRecord
   # Direct associations
 
+  belongs_to :picture
+
   belongs_to :actor
 
   # Indirect associations
