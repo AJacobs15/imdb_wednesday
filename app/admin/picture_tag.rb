33a70a81db9119b30c7e9c1c5ecf5ad87b235ab0
Raghu Betina <raghu@firstdraft.com>
2018-01-03 16:40:51 +0000

    Whitelist columns in PictureTag for ActiveAdmin

--- app/admin/picture_tag.rb
+++ app/admin/picture_tag.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register PictureTag do
 
+ permit_params :actor_id, :picture_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
